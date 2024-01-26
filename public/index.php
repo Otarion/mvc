876c00fbc328a1ba49d39af58f42832c8be2bc13
Otarion <c.garsault@laposte.net>
2024-01-26 16:07:27 +0100

    Création de la class App + Instanciation

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+// Inclure l'autoload de Composer
+require_once __DIR__ . '/../vendor/autoload.php';
+
+//Créer une nouvelle instance de la classe App
+use MVC\app\App;
+$app = App::getInstance();
+
+//Appeler la méthode boot() de la classe App
+$app->boot();
---
+++ src/app.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MVC\app;
+
+class App
+{
+    // Stockage de l'instance de la classe App
+    private static $instance;
+
+    // Rendre le constructeur privé pour empêcher la création de nouvelles instances
+    private function __construct() {}
+
+    // Méthode statique pour obtenir l'instance de la classe App
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    // La méthode boot est vide par défaut
+    public function boot() {}
+}
